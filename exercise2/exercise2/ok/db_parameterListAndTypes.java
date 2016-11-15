02a58980fc9268bb40f735a22ff4c92f1e6d1f97
DanielJohannesBlank <DanielJBlank@users.noreply.github.com>
2016-11-15 14:41:39 +0100

    Add files via upload
    Added fixed or new testcases according to the new file structure

---
+++ exercise2/exercise2/ok/db_parameterListAndTypes.java
@@ -0,0 +1,10 @@
+class A {public static void main(String[] args){
+
+}}
+class BaseFoo {
+    public int returnnumber(int a, int[] b, boolean sotrue, aowntype ownobject){
+    	int a;
+    	a = 2;
+    	return 3;
+    }
+  }
